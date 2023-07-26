da075e480910d7377ae68bd063bf9372f711acd8
Sona <s.harutyunyan1992@gmail.com>
2023-07-26 14:54:00 +0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        BmiService service = new BmiService();
+        int weightInKg = 98;
+        double heightInMeters = 1.87;
+        double myBmi= service.calculate(98,1.87);
+        System.out.println("Масса: "+ myBmi);
+    }
+}
